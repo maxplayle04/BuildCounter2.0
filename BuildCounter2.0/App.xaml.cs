9652258a6133c40bf5837c789f64c42780cce252
Max Playle <maxplayle04@gmail.com>
2021-11-23 19:47:37 +0000

    push changes including icon

--- BuildCounter2.0/App.xaml.cs
+++ BuildCounter2.0/App.xaml.cs
@@ -13,5 +13,17 @@ namespace BuildCounter2._0
     /// </summary>
     public partial class App : Application
     {
+        /// <summary>
+        /// This method is called when the application starts. We're using the bog-standard C# method rather than using a StartupUri 
+        /// incase we want to add any additional thread code or anything later down the line.
+        /// </summary>
+        private void OnApplicationStart(object sender, StartupEventArgs e)
+        {
+
+            MainWindow mw = new MainWindow();
+            mw.Show();
+
+            Current.MainWindow = mw;
+        }
     }
 }
