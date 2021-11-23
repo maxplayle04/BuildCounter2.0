3795a085b2a09a9485299a37c84682dc9a167db4
Max Playle <maxplayle04@gmail.com>
2021-11-23 18:50:35 +0000

    Make changes to the Main Window

--- BuildCounter2.0/MainWindow.xaml.cs
+++ BuildCounter2.0/MainWindow.xaml.cs
@@ -18,7 +18,7 @@ namespace BuildCounter2._0
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    internal partial class MainWindow : Window
     {
         public MainWindow()
         {
